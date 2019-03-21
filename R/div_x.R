9e1996ddec069c08cfa82dc2c1a32de0cf17e2aa
GinerM <giner@cirad.fr>
2019-03-21 16:23:08 +0100

    first commit

---
+++ R/div_x.R
@@ -0,0 +1,16 @@
+#' Divide x
+#'
+#' @description Divide x by y
+#'
+#' @param x Numerator
+#' @param y Denominator
+#'
+#' @return x/y
+#' @export
+#'
+#' @examples
+#' div_x(1,2)
+#'
+div_x= function(x=10,y){
+  x/y
+}
